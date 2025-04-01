8cf260baf9fd416576d08fae6de9b0b0c13b9958
toz12 <thomas@zell-mbc.com>
2025-04-01 15:11:43 +0200

    Forgot to convert reset button

--- app/src/main/java/org/tensorflow/lite/examples/soundclassifier/SettingsActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/soundclassifier/SettingsActivity.java
@@ -6,7 +6,6 @@ import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.Preference;
